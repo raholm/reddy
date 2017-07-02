8021bec0a06e34b8f19bd9535d33f69a819820ba
Rasmus Holm <rasmus.holm723@gmail.com>
2017-07-02 19:42:34 +0200

    Added a few extra filter functions and corresponding tests.

--- reddy/R/transformation.R
+++ reddy/R/transformation.R
@@ -14,5 +14,52 @@ filter_attrs <- function(corpus, attrs=NULL, negative=FALSE) {
         attrs <- names(corpus)[!(names(corpus) %in% attrs)]
     }
 
+    .remove_attrs(corpus, attrs)
+}
+
+#' @keywords internal
+.remove_attrs <- function(corpus, attrs) {
     corpus %>% dplyr::select(-dplyr::one_of(attrs))
+}
+
+#' Filter digits from a corpus
+#'
+#' It does not remove numbers embedded in words such as 'hello123', '123hello', 'h3ll0'.
+#'
+#' @param corpus A dataframe of the corpus
+#' @return A filtered corpus
+#'
+#' @export
+filter_digits <- function(corpus) {
+    assert_subset(names(corpus), "body")
+    .remove_digits(corpus)
+}
+
+#' @keywords internal
+.remove_digits <- function(corpus) {
+    .remove_digits_helper <- function(text) {
+        as.factor(stringr::str_trim(gsub("\\b\\d+\\b", "", text)))
+    }
+
+    corpus %>% dplyr::mutate(body=.remove_digits_helper(body))
+}
+
+#' Filter non-alphanumeric characters from a corpus
+#'
+#' @param corpus A dataframe of the corpus
+#' @return A filtered corpus
+#'
+#' @export
+filter_non_alphanumeric <- function(corpus) {
+    assert_subset(names(corpus), "body")
+    .remove_non_alphanumeric(corpus)
+}
+
+#' @keywords internal
+.remove_non_alphanumeric <- function(corpus) {
+    .remove_non_alphanumeric_helper <- function(text) {
+        as.factor(stringr::str_replace_all(text, "[[:punct:]^]", ""))
+    }
+
+    corpus %>% dplyr::mutate(body=.remove_non_alphanumeric_helper(body))
 }
--- reddy/tests/testthat/test_transformation.R
+++ reddy/tests/testthat/test_transformation.R
@@ -31,3 +31,25 @@ test_that("filter_attrs with negative=TRUE keeps the input attributes", {
 
     expect_true(all(c("body", "author") %in% names(corpus)))
 })
+
+test_that("filter_digits raises an error for invalid input", {
+  expect_error(filter_digits(data.frame()))
+})
+
+test_that("filter_digits removes digits from input", {
+    corpus <- data.frame(body=c("123 hello 123", "123 h3ll0 321a"))
+    actual <- filter_digits(corpus)
+    expected <- data.frame(body=c("hello", "h3ll0 321a"))
+    expect_equal(actual, expected)
+})
+
+test_that("filter_non_alphanumeric raises an error for invalid input", {
+  expect_error(filter_non_alphanumeric(data.frame()))
+})
+
+test_that("filter_non_alphanumeric removes non-alphanumeric characters from input", {
+    corpus <- data.frame(body=c("hello{@\\[}}}^^.,-^'", "there's a bu^gger!"))
+    actual <- filter_non_alphanumeric(corpus)
+    expected <- data.frame(body=c("hello", "theres a bugger"))
+    expect_equal(actual, expected)
+})
