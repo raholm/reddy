b3c2e010077865bb54fdba073f8b293118df6f32
Rasmus Holm <rasmus.holm723@gmail.com>
2017-07-02 15:21:33 +0200

    Functions for reading Reddit comments data.

---
+++ reddy/R/file.R
@@ -0,0 +1,52 @@
+#' Reads a json file containing Reddit comments in the form of a stream.
+#' It assumes that every line in the file is a comment record in the form of a json object.
+#'
+#' @param filename The filename
+#' @param pagesize The number of rows read in each iteration
+#' @return A dataframe containing the Reddit comments
+read_reddit_stream <- function(filename, pagesize=1000, ...) {
+    assert_file_is_json(filename)
+
+    data <- try(jsonlite::stream_in(file(filename), pagesize=pagesize, verbose=FALSE), silent=TRUE)
+
+    .check_reddit_data(data)
+}
+
+#' Reads a json file containing Reddit comments in a single pass.
+#' Assumes that the comments are enclosed in '[]' and separated by ','.
+#'
+#' @note This is very memory hungry. For larger files it is recommended to use \code{\link{read_reddit_stream}} instead.
+#'
+#' @param filename The filename
+#' @return A dataframe constaining the Reddit comments
+read_reddit_raw <- function(filename, ...) {
+    assert_file_is_json(filename)
+
+    data <- try(jsonlite::read_json(filename, simplifyVector = TRUE), silent=TRUE)
+
+    .check_reddit_data(data)
+}
+
+.check_reddit_data <- function(data, ...) {
+    if (!is.data.frame(data)) {
+        error_message <- data[[1]]
+        hint <- ""
+
+        if (grepl("EOF", error_message) |
+            grepl("',' or ']'", error_message)) {
+            hint <- "The error probabily means that the json-objects are not enclosed in '[]' and/or separated by ','"
+        }
+
+        stop(paste(error_message, paste("Hint:", hint, sep=" "), sep="\n"))
+    }
+
+    data
+}
+
+#'
+get_filesize_in_mb <- function(filename, ...) {
+    assert_file_exists(filename)
+
+    bytes <- file.size("filaname")
+    bytes / (1024 * 1024)
+}
