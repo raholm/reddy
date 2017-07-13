e0572f7022909b760f8e994b2efbed17ab17d97a
raholm <rasmus.holm723@gmail.com>
2017-07-13 15:18:15 +0200

    Removed all text curation stuff (moved to texcur package).

--- reddy/tests/testthat/test_transformation.R
+++ reddy/tests/testthat/test_transformation.R
@@ -31,25 +31,3 @@ test_that("remove_attrs with negative=TRUE keeps the input attributes", {
 
     expect_true(all(c("body", "author") %in% names(corpus)))
 })
-
-test_that("remove_digits raises an error for invalid input", {
-  expect_error(remove_digits(data.frame()))
-})
-
-test_that("remove_digits removes digits from input", {
-    corpus <- data.frame(body=c("123 hello 123", "123 h3ll0 321a"), stringsAsFactors=FALSE)
-    actual <- remove_digits(corpus)
-    expected <- data.frame(body=c("hello", "h3ll0 321a"), stringsAsFactors=FALSE)
-    expect_equal(actual, expected)
-})
-
-test_that("remove_non_alphanumeric raises an error for invalid input", {
-  expect_error(remove_non_alphanumeric(data.frame()))
-})
-
-test_that("remove_non_alphanumeric removes non-alphanumeric characters from input", {
-    corpus <- data.frame(body=c("hello{@\\[}}}^^.,-^'", "^^. there's a bu^gger!"), stringsAsFactors=FALSE)
-    actual <- remove_non_alphanumeric(corpus)
-    expected <- data.frame(body=c("hello", "theres a bugger"), stringsAsFactors=FALSE)
-    expect_equal(actual, expected)
-})
